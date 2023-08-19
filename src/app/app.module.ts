ae99f4baaa7443a60e835d9f40eb2cb11640399f
Ananthakumar Vishnurathan <124557188+ananthvishnu@users.noreply.github.com>
2023-08-19 23:52:21 +0530

    standalone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { SharedModule } from './shared/shared.module';
+import { DetailsComponent } from './welcome/details/details.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,WelcomeComponent, DetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
